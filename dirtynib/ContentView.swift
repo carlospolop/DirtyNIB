0c9498ee749bf0a5ab33b998dce789271793757d
carlospolop <carlospolop@gmail.com>
2023-10-10 09:11:23 +0200

    Initial Commit

---
+++ dirtynib/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  dirtynib
+//
+//  Created by Carlos Polop on 10/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ dirtynib/dirtynibApp.swift
@@ -0,0 +1,17 @@
+//
+//  dirtynibApp.swift
+//  dirtynib
+//
+//  Created by Carlos Polop on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct dirtynibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
